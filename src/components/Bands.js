ad7397155913393d0418662b7ebe18e681124b57
Ray Mulligan <rmullig2@optonline.net>
2017-07-21 18:45:01 -0400

    Two tests failing

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import BandInput from './BandInput';
 
 class Bands extends Component {
   render(){
+    const bands = this.props.store.getState().bands.map((band, index) => {
+      return <BandInput text={band.text} key={index} />
+      });
     return (
       <div>
-        band input
+        //<li>{Object.keys(this.props)}</li>
+        <li>{bands}</li>
       </div>
     );
   }
