aa8029eb4e3f8bae8e1664e3552c29040522328c
Mary <mumoh@mozilla.com>
2017-09-19 13:12:58 -0400

    Done.

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Bands extends Component {
   render() {
       const bands = this.props.store.getState().bands.map((band, index) => {
-        return <li key={index}>band</li>
+        return <li key={index}>{band}</li>
     });
 
     return (
