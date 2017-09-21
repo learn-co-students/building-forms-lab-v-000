826a7d10034ab1c0b21a16ffd143fce7deda4751
John Pfingst <jpfingst@outlook.com>
2017-09-20 21:48:51 -0400

    Done.

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -4,7 +4,9 @@ class Bands extends Component {
   render(){
     return (
       <div>
-        band input
+        <ul>
+          {this.props.store.getState().bands.map(band => <li>{band}</li>)}
+        </ul>
       </div>
     );
   }
