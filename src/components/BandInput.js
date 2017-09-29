b5abce6563ff4866f7bbfce20a654db201c4b575
Bob Habig <rfhab2@gmail.com>
2017-09-29 18:03:38 -0400

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
 
 class BandInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: '',
+    };
+  }
+
+  handleChange(event) {
+    this.setState({
+      text: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    this.props.store.dispatch({
+      type: 'ADD_BAND',
+      band: this.state.text,
+    });
+
+    this.setState({
+      text: '',
+    });
+  }
+
   render() {
     return (
       <div>
-        bands component
+        <form onSubmit={(event) => this.handleSubmit(event) } >
+          <p>
+            <label>Add Band Name: </label>
+            <input type='text'
+                   onChange={(event) =>  this.handleChange(event) }
+                   value={ this.state.text }/>
+          </p>
+          <input type='submit' />
+        </form>
       </div>
     );
   }
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,9 +1,7 @@
-export default function manageBand(state = {
-  bands: [],
-}, action) {
+export default function manageBand(state = { bands: [], }, action) {
   switch (action.type) {
     case 'ADD_BAND':
-      return Object.assign({}, state, { bands: state.bands.concat(action.band) });
+      return { bands: state.bands.concat(action.band) };
     default:
       return state;
   }
