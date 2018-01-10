ab63136e1e4a3cfc1e2c217ee28f22118b9fe3a5
Jim Stricker <jmstricker93@gmail.com>
2018-01-10 09:48:52 -0500

    text input component is now successfully a controlled component

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 class BandInput extends Component {
+
+  constructor(){
+    super()
+    this.state = {
+      text: ''
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      text: e.target.value
+    })
+  }
+
+
   render() {
     return (
       <div>
-        bands component
+        <form>
+          <input type='text' onChange={this.handleChange} value={this.state.text} />
+          <input type='submit' />
+        </form>
+        {this.state.text}
       </div>
     );
   }
