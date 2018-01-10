2be799a5158e9a1c7970621d151fa93fe0a9279f
Jim Stricker <jmstricker93@gmail.com>
2018-01-10 12:24:34 -0500

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -15,11 +15,22 @@ class BandInput extends Component {
     })
   }
 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_BAND',
+      band: this.state.text
+    })
+    this.setState({
+      text: ''
+    })
+  }
+
 
   render() {
     return (
       <div>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input type='text' onChange={this.handleChange} value={this.state.text} />
           <input type='submit' />
         </form>
