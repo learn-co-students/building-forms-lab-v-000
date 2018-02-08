81a801878415ff6d87cdd74851ca898c58c00ff9
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-02-08 00:40:24 -0800

    wont pass built in tests, but refactored to display each band as a Component of Band

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-// import Band from './Band';
+import Band from './Band'; //version where I turn Band into component
 class Bands extends Component {
   render(){
     // debugger;
     const bands = this.props.store.getState().bands.map((band, index) => {
-      // return <Band band={band} key={index} />
-      return <li key={index}>{band}</li>
+      return <Band band={band} key={index} />
+      // return <li key={index}>{band}</li>
     })
     return (
       <div>
