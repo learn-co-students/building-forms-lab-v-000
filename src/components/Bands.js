22d524ef28d979f9d48dd25246089acbd9ce04ff
satkins16 <satkins16@gmail.com>
2018-03-26 22:15:47 -0400

    Done.

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 
 class Bands extends Component {
   render(){
+
+    const bands = this.props.store.getState().bands.map((band, index) => {
+      return <li>{band}</li>
+    })
+
     return (
       <div>
-        band input
+        {bands}
       </div>
     );
   }
