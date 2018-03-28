605a263e5bea3be659cd76788bc770aad8dc26d6
pbhagyashri <bhagyashri.padmawar@gmail.com>
2018-03-28 11:56:42 -0400

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,10 +1,13 @@
 export default function manageBand(state = {
   bands: [],
 }, action) {
+  
   switch (action.type) {
     case 'ADD_BAND':
-      return Object.assign({}, state, { bands: state.bands.concat(action.band) });
+      return { bands: state.bands.concat(action.band.text)};
     default:
       return state;
   }
 };
+
+//Object.assign({}, state
