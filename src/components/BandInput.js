b9c5b06723209a8b0603b8ced3cac6c65a3454a7
Hannah Carnes <hecarnes@gmail.com>
2018-03-28 21:38:19 -0400

    initial state set to empty string

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 
 class BandInput extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+    };
+  }
+
   render() {
     return (
       <div>
