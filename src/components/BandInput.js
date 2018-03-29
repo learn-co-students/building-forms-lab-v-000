8d74bdc02c21b536c786a279925a7aefe49c5388
jss530 <jsifre01@gmail.com>
2018-03-28 20:50:27 -0400

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
 
 class BandInput extends Component {
-  render() {
-    return (
-      <div>
-        bands component
-      </div>
-    );
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+    };
   }
-};
+
+  handleKeydown(event) {
+    this.setState({
+      text: event.target.value
+    });
+  };
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_BAND',
+      band: this.state.text,
+    });
+    this.setState({
+      text: '',
+    });
+  }
+
+  render(){
+     return(
+       <div>
+         <form onSubmit={(event) => this.handleSubmit(event)}>
+           <p>
+             <label>Add your band name below!</label>
+             <input
+              type="text"
+              value={this.state.text}
+              onChange={(event) => this.handleKeydown(event)} />
+           </p>
+           <input type="submit" />
+         </form>
+       </div>
+     );
+   }
+}
 
 export default BandInput;
--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
 class Bands extends Component {
-  render(){
-    return (
-      <div>
-        band input
-      </div>
+  render() {
+
+    const bands = this.props.store.getState().bands.map((band, index) => {
+      return <li key={index}>{ band }</li>
+    });
+
+    return(
+      <ul>
+        {bands}
+      </ul>
     );
   }
 };
