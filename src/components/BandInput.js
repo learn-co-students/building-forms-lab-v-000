c545139ab601abe4f453e78ba76ff7b3c387b3e1
PRbsas <paularamirez0316@gmail.com>
2018-04-07 13:25:34 -0400

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,13 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class BandInput extends Component {
-  render() {
+  constructor () {
+    super()
+
+    this.state = {
+      text: ''
+    }
+  }
+
+  handleChange (event) {
+    this.setState({
+      text: event.target.value
+    })
+  }
+
+  handleSubmit (event) {
+    event.preventDefault()
+    this.props.store.dispatch({
+      type: 'ADD_BAND',
+      band: this.state.text
+    })
+    this.setState({
+      text: ''
+    })
+  }
+
+  render () {
     return (
       <div>
-        bands component
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <p>
+            <label>add band</label>
+            <input
+              type='text'
+              value={this.state.text}
+              onChange={(event) => this.handleChange(event)}
+            />
+          </p>
+          <input type='submit' />
+        </form>
       </div>
-    );
+    )
   }
-};
+}
 
-export default BandInput;
+export default BandInput
--- src/createStore.js
+++ src/createStore.js
@@ -1,25 +1,25 @@
-import { renderer } from './index.js';
+import { renderer } from './index.js'
 
-export default function createStore(reducer){
-  let state;
+export default function createStore (reducer) {
+  let state
 
-  function dispatch(action) {
-    state = reducer(state, action);
+  function dispatch (action) {
+    state = reducer(state, action)
     if (process.env.NODE_ENV !== 'test') {
-      console.log(`the action is`);
-      console.log(action);
-      console.log(`the state is`);
-      console.log(state);
+      console.log(`the action is`)
+      console.log(action)
+      console.log(`the state is`)
+      console.log(state)
     }
     renderer.render()
-  };
+  }
 
-  function getState(){
-    return state;
-  };
+  function getState () {
+    return state
+  }
 
   return {
-    dispatch, 
+    dispatch,
     getState
-  };
-};
+  }
+}
