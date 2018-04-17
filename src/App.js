bf51480d2dc241169330b34b9378cfbd9e7eadd3
Joseph Surla <josephsurla@gmail.com>
2018-04-16 17:00:34 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import BandInput from './components/BandInput';
-import Bands from './components/Bands';
+import React, { Component } from "react";
+import BandInput from "./components/BandInput";
+import Bands from "./components/Bands";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <BandInput store={this.props.store} />
-        <Bands store={this.props.store}/>
+        <Bands store={this.props.store} />
       </div>
     );
   }
-};
+}
 
 export default App;
--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,13 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class BandInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ""
+    };
+  }
+
+  handleChange = e => {
+    this.setState({ text: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.store.dispatch({
+      type: "ADD_BAND",
+      band: this.state.text
+    });
+    this.setState({ text: "" });
+  };
+
   render() {
     return (
       <div>
-        bands component
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label>Add Band:</label>
+            <br />
+            <input
+              type="text"
+              onChange={event => this.handleChange(event)}
+              value={this.state.text}
+            />
+          </div>
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </form>
       </div>
     );
   }
-};
+}
 
 export default BandInput;
