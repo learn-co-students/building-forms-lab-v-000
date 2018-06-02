6961a7225608aede50d4706848517eca58bb3053
Sophia Varella <sophiavarella@gmail.com>
2018-06-01 19:02:37 -0700

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,13 +1,55 @@
 import React, { Component } from 'react';
 
 class BandInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: ''
+    };
+  }
+
+  handleChange(event) {
+    this.setState({
+      text: event.target.value
+    });
+  };
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.store.dispatch({
+      type: 'ADD_BAND',
+      band: this.state.text,
+    });
+    this.setState({
+      text: '',
+    });
+  };
+
   render() {
     return (
       <div>
-        bands component
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <p>
+            <label>add band</label>
+            <input type="text" value={this.state.text} onChange={(event) => this.handleChange(event)} />
+          </p>
+          <input type="submit" />
+        </form>
       </div>
     );
   }
 };
 
 export default BandInput;
+
+
+//form element with onSubmit handler
+  // input text, value, onChange
+  // submit button
+
+  // handleChange
+  //handleSubmit
+    // 1.preventDefault,
+    // 2. dispatch action
+    // 3. reset input value - by reseting state
