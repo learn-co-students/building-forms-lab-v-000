bdfc828548158656a862752e06e34923cbad641e
Fachastorm <rodrigo.lodeiro@live.com>
2018-06-11 12:06:14 -0400

    Done.

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 
 class Bands extends Component {
+ 
+ 
   render(){
-    return (
-      <div>
-        band input
-      </div>
-    );
+      const bands = this.props.store.getState().bands.map((band, index) => { 
+        return <li key={index}>{band}</li>
+      });
+
+      return (
+        <div>
+        {bands}
+        </div>
+      )
+
   }
 };
 
