005cbee762010b307979d052115abc36a60604bd
yahyal0v3 <yahyal0v3@yahoo.com>
2018-09-01 12:59:53 -0700

    Done.

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react'
+import Band from './Band'
 
-const Bands = props => {
-  const bands = props.bands.map((band, index) => {
-    return <li key={index}>{band.name}</li>;
-  });
+export default class Bands extends Component {
 
-  return (
-    <div>
-      {bands}
-    </div>
-  );
-
-};
-
-export default Bands;
+    render() {
+        const renderBand = this.props.bands.map(band => <Band band={band}/>)
+        return(
+            <div>
+                <ul>
+                    {renderBand}
+                </ul>
+            </div>
+        )
+    }
+}
