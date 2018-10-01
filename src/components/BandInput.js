bef6b733cf6a8d0a5b5bc04cba1f209e352c77a1
Jonathan Tiede <jon@Jonathans-MacBook-Pro.local>
2018-10-01 10:50:21 -0600

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,14 +1,47 @@
 // Add BandInput component
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class BandInput extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: ""
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.addBand(this.state);
+    this.setState({
+      name: ""
+    });
+  };
+
   render() {
-    return(
+    return (
       <div>
-        Band Input
+        <form action="">
+          <label htmlFor="">Add Band: </label>
+          <input
+            type="text"
+            value={this.state.name}
+            name="name"
+            onChange={this.handleChange}
+          />
+          <input type="submit" onClick={this.handleSubmit} />
+        </form>
       </div>
-    )
+    );
   }
 }
 
-export default BandInput
+// const mapDispatchToProps = dispatch => {};
+
+// export default connect()(BandInput);
+export default BandInput;
