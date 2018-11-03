85d9b3112e068ef65c4d9f1b0bd8257584755bfb
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-11-03 04:42:13 +0000

    Done.

---
+++ src/components/Bands.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Bands extends Component {
+  render(){
+    const bands = this.props.store.getState().bands.map((band, index) => {
+      return <li key={index}>{band}</li>
+    });
+
+    return(
+      <ul>
+        {bands}
+      </ul>
+    );
+  }
+};
+
+export default Bands;
