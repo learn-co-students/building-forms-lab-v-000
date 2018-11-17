16871472366411bc23a055ed34a9b4124bb119d0
Deborah Seow <deborahseow9@gmail.com>
2018-11-16 18:10:04 -0600

    Done.

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -31,8 +31,8 @@ export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer)
 //If something is outside the render, the 'this' is the component it is in (ie BandsContainer)
 //If it is defined inside the render(), then it does not need {this.listBands()}. Instead, you can write {listBands()}.
 //Inside the render, 'this' is still BandsContainer, and it does have the same props as outside the render. 'This' also has access to listBands().
-//Cannot declare varaibles inside render()
+//Cannot declare varaibles inside return()
 //EVERYTHING underneath 'render' must be declared
-//It will not hit the function if it is above the render for some reason
+//It will not hit the function if it is above the render for some reason (not sure about that - look at redux-delete-codealong)
 // You will need to write the BandsContainer component. This component should act as your connection to the Redux store and should be a child of the App component.
 // Once you've got a form successfully dispatching actions and adding bands to your Redux store, write the necessary code to display each band name from the store. Band names should be listed in individual li elements.
