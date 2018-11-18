10f320b7bb986fba7cea0c7b5df86a643b4f7c3c
urnotjessie <zg2220@columbia.edu>
2018-11-17 22:46:45 -0500

    Done.

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import BandInput from '../components/BandInput';
 
 class BandsContainer extends Component {
   render() {
     return(
       <div>
-        BandsContainer
+        <BandInput />
       </div>
     )
   }
