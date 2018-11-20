5d0a1ee5c0d210baaf3b81de0ecc18ee18f1bf5b
Sebastian <sebgertz@gmail.com>
2018-11-19 22:17:52 -0500

    all tests passing, works in browser

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,26 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import BandInput from '../components/BandInput.js';
+import BandList from '../components/BandList.js';
+import { connect } from 'react-redux';
 
 class BandsContainer extends Component {
   render() {
     return(
       <div>
-        BandsContainer
+        <BandInput addBand={this.props.addBand}/>
+        <BandList bands={this.props.bands}/>
       </div>
     )
   }
 }
 
-export default BandsContainer
+const mapStateToProps = (state) => {
+  return {
+    bands: state.bands
+  }
+}
+
+// pass mapDispatchToProps down from Container Component to presentational/form component
+const mapDispatchToProps = dispatch => ({ addBand: band => dispatch({ type: "ADD_BAND", band }) })
+
+export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer)
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,5 +1,11 @@
 export default function manageBand(state = {
-  bands: [],
+  bands: []
 }, action) {
-  return state
+  switch (action.type) {
+    case 'ADD_BAND':
+      // make copy of state object, change band key to copy of bands plus payload
+      return { ...state, bands: [...state.bands, action.band ] }
+    default:
+      return state;
+  }
 };
