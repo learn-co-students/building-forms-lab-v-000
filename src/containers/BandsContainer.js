e5152a6989f5caeb5157c8c5d70fc7b9b2d24499
Mallory <mallory.feuer@gmail.com>
2019-02-21 20:33:22 -0500

    Done.

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -9,7 +9,7 @@ class BandsContainer extends Component {
         BandsContainer
         <BandInput bands={this.props.addBand}/>
         {this.props.bands.map((band, i) =>
-          <li>{band}</li>
+          <li>{band.name}</li>
         )}
       </div>
     )
