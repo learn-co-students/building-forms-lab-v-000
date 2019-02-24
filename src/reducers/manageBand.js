7983b03043618d813e0452aa33194fbd0964e1ae
github username <github email address>
2019-02-23 19:33:19 -0500

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,5 +1,11 @@
-export default function manageBand(state = {
-  bands: [],
-}, action) {
-  return state
+export const addBandName = (bandName) => {
+  return {type: 'ADD_BAND',bandName}
+}
+export default function manageBand(state = {bands: []}, action) {
+  switch(action.type){
+    case 'ADD_BAND':
+      return {...state,bands: [...state.bands,{name: action.bandName}]}
+
+    default: return state
+  }
 };
