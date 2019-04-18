9f0c0b820d02517f64a09f2cffbbcbdecf5aeb6e
github username <github email address>
2019-04-17 22:08:54 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import BandsContainer from './containers/BandsContainer';
 
+
 class App extends Component {
   render() {
     return (
--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -2,13 +2,42 @@
 import React, { Component } from 'react'
 
 class BandInput extends Component {
+  state = {
+    name: ''
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state)
+    this.props.addBand(this.state)
+
+    this.setState({
+      name: ''
+    })
+
+  }
+
+  handleChange = event => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
   render() {
     return(
       <div>
-        Band Input
+        <form onSubmit={this.handleSubmit}>
+    	    <label>add a band </label>
+          <input
+            type="text" 
+            onChange={this.handleChange} 
+            value={this.state.name}
+            />
+          <input type="submit" />
+       </form>
       </div>
     )
   }
 }
 
-export default BandInput
+export default BandInput;
