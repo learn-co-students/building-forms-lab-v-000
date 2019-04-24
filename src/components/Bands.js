bd0ac4f3458ec3883e6eb57aa46c06cd7b6e9f24
emanuelbierman <emanuel.bierman@gmail.com>
2019-04-24 11:23:18 -0400

    Done.

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Bands = props => {
   return(
     <ul>
-    {props.bands.map(band => <li>{band.name}</li> )}
+    {props.bands.map((band, index) => <li key={index}>{band.name}</li> )}
     </ul>
   );
 };
