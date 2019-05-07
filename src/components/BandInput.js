02b794ce63694eca4139ba4c9e6806188f17af00
julianjohannesen <github email address>
2019-05-07 14:33:53 -0400

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,14 +1,48 @@
 // Add BandInput component
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addBand } from '../actions/actions.js';
 
 class BandInput extends Component {
-  render() {
-    return(
-      <div>
-        Band Input
-      </div>
-    )
-  }
+
+	state = {
+		name: '',
+	}
+
+	handleChange = (e) => {
+		this.setState({
+			name: e.target.value,
+		});
+	}
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+		this.props.addBandMapped({ name: this.state.name });
+		this.setState({ name: '' });
+	}
+
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.handleSubmit}>
+					<input
+						name="bandName"
+						onChange={this.handleChange}
+						placeholder="Enter a band name."
+						type="text"
+						value={this.state.name}
+					/>
+					<button type="submit">Submit</button>
+				</form>
+			</div>
+		)
+	}
 }
 
-export default BandInput
+const mapDispatchToProps = dispatch => {
+	return {
+		addBandMapped: band => dispatch(addBand(band)),
+	}
+}
+
+export default connect(null, mapDispatchToProps)(BandInput)
--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import uuid from 'uuid';
 
 class BandsContainer extends Component {
+
+  generateBandsList = () => {
+    console.log("From inside the generateBandsList function: ", this.props)
+    return this.props.bands.map( band => {
+      return (
+        <li key={uuid()}>{band.name}</li>
+      )
+    })
+  }
+
   render() {
+    console.log("From inside the render function: ", this.props)
     return(
       <div>
-        BandsContainer
+        <ul>
+        {this.props.bands ? this.generateBandsList() : null}
+        </ul>
       </div>
     )
   }
+
 }
 
-export default BandsContainer
+const mapStateToProps = state => {
+  return {
+    bands: state.bands // this is an array of band objects
+  }
+}
+
+export default connect(mapStateToProps)(BandsContainer)
