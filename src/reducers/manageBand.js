3a4fa5f161c4a0862cc3201015d200cf1da310ef
Rollin <rollin.metzger@gmail.com>
2019-05-15 18:25:17 -0500

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,5 +1,11 @@
 export default function manageBand(state = {
   bands: [],
 }, action) {
-  return state
+  switch (action.type) {
+    case 'ADD_Band':
+      return { bands: state.bands.concat(action.band) };
+
+    default:
+      return state;
+  };
 };
