eb47acfe720695a7648c249203fd8cc358e921fd
Ryan <ryanwevans@icloud.com>
2019-06-17 14:23:34 -0700

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -2,13 +2,40 @@
 import React, { Component } from 'react'
 
 class BandInput extends Component {
+  state = {
+    name: ''
+  };
+
+  handleOnChange (event) {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  handleOnSubmit (event) {
+    event.preventDefault();
+    this.props.addBand(this.state);
+    this.setState({
+      name: ''
+    })
+  }
+
   render() {
     return(
       <div>
-        Band Input
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+              <label>Band Name:</label>
+              <input
+                name="name"
+                onChange={(event) => this.handleOnChange(event)}
+                type="text"
+                value={this.state.name} />
+              <input type="submit" />
+        </form>
       </div>
     )
   }
+
 }
 
-export default BandInput
+export default BandInput;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import manageBand from './reducers/manageBand'
 
-const store = createStore(manageBand)
+const store = createStore(manageBand);
 
 
 ReactDOM.render(
