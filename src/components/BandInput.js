40fc09ced49cc22b8ddf5cc752ed2224a6195467
harleyharl <harleymteden@gmail.com>
2019-06-25 11:00:11 -0700

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,14 +1,53 @@
-// Add BandInput component
+import { connect } from 'react-redux'
+// import { addBand } from '../reducers/manageBand'
 import React, { Component } from 'react'
 
 class BandInput extends Component {
+  //
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+    };
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addBand(this.state)
+  }
+
+  handleChange = event => {
+    this.setState({
+        name: event.target.value
+    });
+  };
+
   render() {
     return(
       <div>
-        Band Input
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <label>Add Band</label>
+            <input type="text" onChange={this.handleChange} value={this.state.name}/>
+          </p>
+          <input type="submit" />
+        </form>
+        {this.state.name}
       </div>
     )
   }
 }
 
-export default BandInput
+const mapStateToProps = state => {
+  return {
+    bands: state.bands
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        addBand: formData => dispatch({ type: 'ADD_BAND', payload: formData })
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(BandInput)
--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
+import BandInput from '../components/BandInput';
+import { connect } from 'react-redux';
 
 class BandsContainer extends Component {
+
+  renderBands = () => this.props.bands.map((band, index) => <li key={index}> band </li>)
+
   render() {
     return(
       <div>
-        BandsContainer
+        <BandInput />
+        {this.renderBands()}
       </div>
     )
   }
 }
 
-export default BandsContainer
+const mapStateToProps = state => {
+  return {
+    bands: state.bands
+  }
+}
+
+export default connect(mapStateToProps)(BandsContainer);
