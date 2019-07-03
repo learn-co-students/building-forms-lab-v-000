64275f866164d9e4004ac5e424b9775650726fe7
roitblatari <roitblatari@gmail.com>
2019-07-03 13:57:32 -0400

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,14 +1,35 @@
 // Add BandInput component
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class BandInput extends Component {
-  render() {
-    return(
-      <div>
-        Band Input
-      </div>
-    )
-  }
+	state = {
+		name: ''
+	};
+
+	handleOnChange = (event) => {
+		// Handle Updating Component State
+		this.setState({
+			[event.target.name]: event.target.value
+		});
+	};
+
+	handleOnSubmit = (e) => {
+		e.preventDefault();
+		this.setState({
+			name: ''
+		});
+	};
+	render() {
+		return (
+			<form onSubmit={this.handleOnSubmit}>
+				<label htmlFor="band-input"> Band Input</label>
+
+				<input onChange={this.handleOnChange} name="band-input" type="text" />
+				<button type="submit">Submit </button>
+			</form>
+		);
+	}
 }
 
-export default BandInput
+export default connect()(BandInput);
--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import BandInput from '../components/BandInput';
 
 class BandsContainer extends Component {
-  render() {
-    return(
-      <div>
-        BandsContainer
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				BandsContainer
+				<BandInput />
+			</div>
+		);
+	}
 }
 
-export default BandsContainer
+export default BandsContainer;
