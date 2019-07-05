8e0729975049d47a20d6cf6342a7801477c35dd5
github username <github email address>
2019-07-05 16:20:01 -0500

    ran npm install

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class BandsContainer extends Component {
   render() {
@@ -10,4 +11,4 @@ class BandsContainer extends Component {
   }
 }
 
-export default BandsContainer
+export default connect()(BandsContainer);
