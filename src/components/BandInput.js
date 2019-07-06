491898133e70ff641522eeec77d1150af6a3473f
github username <github email address>
2019-07-06 17:24:00 -0500

    save new file tests not passing

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -1,14 +1,39 @@
 // Add BandInput component
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class BandInput extends Component {
+
+	state = {
+		name: ''
+	}
+
+	handleOnChange(event) {
+		this.setState({
+			name: event.target.value,
+	});
+}
+	handleOnSubmit(event) {
+		event.preventDefautl();
+		this.props.addBand(this.state)
+		this.setState({
+			name: '',
+		});
+	}
+	
   render() {
     return(
       <div>
-        Band Input
+      <form onSubmit={(event) => this.handleOnSubmit(event)}>
+      <label>Add Band:</label>
+      <input type="text"
+      onChange={(event) => this.handleOnChange(event)}
+      value={this.state.name}
+      />
+      <input type="submit"/>
+      </form>
       </div>
-    )
+    );
   }
-}
+};
 
-export default BandInput
+export default BandInput;
