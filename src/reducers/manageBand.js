670e1413d4d6999ce1866f6f9866d6358a893082
Lara Sanders <larasanders@gmail.com>
2019-07-07 15:14:10 +0000

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -4,7 +4,7 @@ export default function manageBand(state = {
   switch (action.type) {
     case 'ADD_BAND':
 
-    return { bands: state.bands.concat(action.payload.text) };
+    return [...state, action.band];
 
     default:
       return state
