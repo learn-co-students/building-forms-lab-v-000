a1a8a7250eee43e3f608da6a635b07df4cbb1fb3
sammy-brake <sammy.brake@gmail.com>
2019-07-17 07:45:37 -0500

    Done.

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import BandInput from '../components/BandInput'
+import Bands from '../components/Bands'
 
 class BandsContainer extends Component {
   render() {
     return(
       <div>
         <BandInput addBand={this.props.addBand} />
-        {/* <Bands bands={this.props.bands} /> */}
+        <Bands bands={this.props.bands} />
         
       </div>
     )
