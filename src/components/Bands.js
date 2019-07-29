e9cb6b73b98a344b7444163e87f7601c21356307
github username <github email address>
2019-07-29 11:34:31 -0400

    Done.

---
+++ src/components/Bands.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+const Bands = props => {
+
+  const bands = props.bands.map((band,index) => {
+
+    return <li key={index}> {band} </li>
+
+  })
+return (<div>{bands}</div>)
+}
+
+
+export default Bands
