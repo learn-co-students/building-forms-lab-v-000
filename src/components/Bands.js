003a791968c2d60faa53fd1f93a43e6d5398e10d
github username <github email address>
2019-07-29 11:37:01 -0400

    Done.

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -4,7 +4,7 @@ const Bands = props => {
 
   const bands = props.bands.map((band,index) => {
 
-    return <li key={index}> {band} </li>
+    return <li key={index}> {band.name} </li>
 
   })
 return (<div>{bands}</div>)
