7830551211eb3001a2e60bcd5857352ad4688b4e
Wendy Calderon <wendycalderon@gmail.com>
2019-08-25 18:29:12 -0400

    Add initial state and form to BandInput

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -2,13 +2,26 @@
 import React, { Component } from 'react'
 
 class BandInput extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+    };
+  }
+
   render() {
     return(
       <div>
-        Band Input
+        <form onSubmit={this.handleSubmit}>
+          <label>Band Input: </label>
+          <input type="text" onChange={(event) => this.handleChange(event)} value={this.state.text}/>
+          <input type="submit" />
+        </form>
       </div>
     )
   }
 }
 
 export default BandInput
+
+
