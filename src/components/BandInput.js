1493c76eb49b77c5c551a032967c9a08f53cff58
Jason Mei <jason.mei403@gmail.com>
2020-02-13 23:13:49 -0500

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -24,7 +24,7 @@ class BandInput extends Component {
     return(
       <div>
         <form onSubmit={this.handleOnSubmit}>
-          <input type='text' value={this.state.text} onChange={this.handleOnChange}/>
+          <input type='text' value={this.state.name} onChange={this.handleOnChange}/>
           <input type='submit'/>
         </form>
       </div>
