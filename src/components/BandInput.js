1e6a692793c7473e2d95658e260cb64a1225781c
github username <github email address>
2020-03-17 15:05:40 -0400

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -2,10 +2,32 @@
 import React, { Component } from 'react'
 
 class BandInput extends Component {
+  state = {
+   name: ''
+ }
+
+ handleSubmit = event => {
+   event.preventDefault();
+   this.props.addBand(this.state)
+   this.setState({
+     name: '',
+   })
+ }
+
+ handleChange = event => {
+   this.setState({
+     name: event.target.value
+   })
+ }
+
   render() {
     return(
       <div>
-        Band Input
+        <form onSubmit={this.handleSubmit}>
+          <label>Band Input: </label>
+          <input onChange={this.handleChange} value={this.state.name}/>
+          <input type="submit" />
+        </form>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'raf/polyfill';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import manageBand from './reducers/manageBand'
