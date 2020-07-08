8021727890b6f9cebf2d2f1e5677199e9dee8edc
leothebeagle <a.kosber@gmail.com>
2020-07-07 20:14:27 -0400

    Done.

---
+++ src/components/BandCard.js
@@ -0,0 +1,9 @@
+// this is the individual Band li.
+
+// a function component will do here.
+import React from 'react';
+
+const BandCard = (props) => <li>{props.band.name}</li>
+
+
+export default BandCard
--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react'; 
+import BandInput from '../components/BandInput'
+import { addBand } from '../actions/bands';
+import { connect } from 'react-redux';
+import BandsDisplay from '../components/BandsDisplay'
 
 class BandsContainer extends Component {
+  
+
   render() {
+    // console.log("the props that <BandContainer /> receives:")
+    // console.log(this.props)
+    // console.log(this.props)
+    // console.log(this.props.addBand)
+    
     return(
       <div>
-        BandsContainer
+        <BandInput addBand={this.props.addBand}/>
+        <BandsDisplay bands={this.props.bands}/>
       </div>
     )
   }
 }
 
-export default BandsContainer
+const mapStateToProps = state => {
+  // console.log('the state that mapStateToProps gets from the Redux store:')
+  // console.log(state)
+  // console.log('---------')
+  return {
+    bands: state.bands
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addBand: (band) => dispatch(addBand(band))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer)
