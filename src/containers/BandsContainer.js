a1e26c27ad588d85e4a0b475948b8477a54cee02
yuanxizhang <yuanxi.zhang@gmail.com>
2020-08-09 03:03:16 -0700

    Done.

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import BandCard from '../components/BandCard';
 
 class BandsContainer extends Component {
-  render() {
-    return(
-      <div>
-        BandsContainer
-      </div>
-    )
-  }
+		render() {
+		    const { bands, removeBamd } = this.props;
+		    return (
+		      <div>
+		        <hr />
+		        <div className="row justify-content-center">
+		          <h2>Bands</h2>
+		        </div>
+		        <hr />
+		        <div className="container">
+		          <div className="row">
+		            <div className="col-md-4">
+		              {bands.map(band => <BandCard key={band.id} removeBand={removeBamd} band={band} />)}
+		            </div>
+		          </div>
+		        </div>
+		      </div>
+		    );
+		  }
 }
 
-export default BandsContainer
+const mapStateToProps = state => {
+  return { bands: state.bands }
+}
+
+export default connect(mapStateToProps, { removeBand})(Bands); 
