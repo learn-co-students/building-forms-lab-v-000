be5d22577d3253561b75410bb7dec65c27abd9af
TylerSell <41409967+TylerSell@users.noreply.github.com>
2020-09-25 10:57:17 -0500

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -2,10 +2,33 @@
 import React, { Component } from 'react'
 
 class BandInput extends Component {
+  state = {
+    name: ''
+  }
+
+  handleOnChange = (event) => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+    this.props.addBand(this.state);
+    this.setState({
+      name: ''
+    })
+
+  }
+
   render() {
     return(
       <div>
-        Band Input
+        <form onSubmit={this.handleOnSubmit}>
+          <label>Band Name</label>
+          <input type="text" onChange={this.handleOnChange} value={this.state.name}></input>
+          <input type="submit" />
+        </form>
       </div>
     )
   }
--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,5 +1,13 @@
 export default function manageBand(state = {
   bands: [],
 }, action) {
-  return state
+  //console.log(action)
+  //debugger
+  switch(action.type){
+    case'ADD_BAND': 
+    return {...state, bands: [...state.bands, action.payload]}
+
+    default: 
+    return state
+  }
 };
