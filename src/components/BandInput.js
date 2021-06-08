543fd9281c908756b10dab67400cf66778afe12a
emanao <emanao@me.com>
2021-06-08 17:12:24 -0400

    BandInput with handleOnChange

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -2,13 +2,26 @@
 import React, { Component } from 'react'
 
 class BandInput extends Component {
-  render() {
-    return(
-      <div>
-        Band Input
-      </div>
-    )
-  }
+    state={
+        bandName: ""
+    }
+    handleChange=(event)=>this.setState({[event.target.name]:event.target.value})
+
+    render() {
+        return(
+            <div>
+            {/* Band Input */}
+            <form onSubmit={this.handleSubmit} >
+                <input 
+                type="text" 
+                name="bandName" 
+                onChange={this.handleChange}
+                />
+                <button type="submit">Submit</button>
+            </form>
+            </div>
+        )
+    }
 }
 
 export default BandInput
--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import BandInput from '../components/BandInput'
 
 class BandsContainer extends Component {
   render() {
     return(
       <div>
-        BandsContainer
+        {/* BandsContainer */}
+        <BandInput/>
       </div>
     )
   }
