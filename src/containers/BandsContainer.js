932514f73e7e983307d3e72ddffde9a5a8508b3d
github username <github email address>
2021-06-08 16:01:38 -0400

    Done.

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react'
+import BandInput from '../components/BandInput'
+import { connect } from 'react-redux';
+import Bands from '../components/Bands'
 
+//connection to the Redux store 
+//child of App component
 class BandsContainer extends Component {
+ 
   render() {
     return(
       <div>
-        BandsContainer
+        <BandInput addBand={this.props.addBand} />
+        <Bands bands={this.props.bands}/>
       </div>
     )
   }
 }
 
-export default BandsContainer
+const mapStateToProps = (state) => {
+  return { bands: state.bands };
+};
+
+const mapDispatchToProps = dispatch => ({ addBand: band => dispatch({ type: "ADD_BAND", band }) })
+
+export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer);
+
+//This component should act as your connection to the Redux store 
+//and should be a child of the App component. 
+//Any values we want to use from Redux as well as any actions we want to dispatch 
+//should be set up and available in BandsContainer 
+//so they can be passed down to other components.
