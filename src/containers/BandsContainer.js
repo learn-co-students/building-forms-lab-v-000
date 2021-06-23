dc1985a26af4e60b9674ba916a7b820b5b8ab4df
Joeycho <joeyadamcho@gmail.com>
2021-06-23 02:22:14 +0200

    Done.

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import BandInput from '../components/BandInput'
+
 
 class BandsContainer extends Component {
+  
+
+  renderBands = () => this.props.bands.map((band) =>{return(<li>{band.name}</li>)})
+  
   render() {
+    debugger
     return(
       <div>
-        BandsContainer
+        <BandInput addBand={this.props.addBand}/>
+        {this.renderBands()}
       </div>
     )
   }
 }
 
-export default BandsContainer
+const mapDispatchToProps = dispatch => {
+  return {
+    addBand: formData => dispatch({ type: 'ADD_BAND', payload: formData })
+  };
+};
+
+const mapStateToProps = state => {
+  return {
+    bands: state.bands
+  }
+}
+ 
+export default connect(mapStateToProps,mapDispatchToProps)(BandsContainer);
+
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const store = createStore(manageBand)
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <App store={store}/>
   </Provider>,
   document.getElementById('root')
 );
